6c42c869c015560f7a610dd4c3f390d56a017dd0
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2025-09-19 16:38:13 +0400

    Update dependencies to support CUDA 12.8 (#16)

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,7 +4,7 @@ PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 
 docker build --no-cache \
   -f "${SCRIPT_DIR}/Dockerfile.deploy" \
-  -t supervisely/deim:1.0.13-deploy \
+  -t supervisely/deim:1.0.15-deploy \
   "$PROJECT_ROOT"
 
-docker push supervisely/deim:1.0.13-deploy
+docker push supervisely/deim:1.0.15-deploy
