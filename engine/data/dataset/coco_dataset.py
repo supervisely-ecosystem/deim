4802e7d242b34452403243e8ce398bd825461424
cxnt <pavelbartsits@gmail.com>
2025-11-28 13:50:31 +0000

    Remap category2label in prepare method

--- engine/data/dataset/coco_dataset.py
+++ engine/data/dataset/coco_dataset.py
@@ -52,7 +52,7 @@ class CocoDetection(torchvision.datasets.CocoDetection, DetDataset):
         if self.remap_mscoco_category:
             image, target = self.prepare(image, target, category2label=mscoco_category2label)
         else:
-            image, target = self.prepare(image, target)
+            image, target = self.prepare(image, target, category2label=self.category2label)
 
         target['idx'] = torch.tensor([idx])
 
